d2ce672425b4437e124e5dc9cc262d4b1be7c8e7
RyRch <reda.rouich@epitech.eu>
2022-03-08 17:01:39 +0100

    [UPD] fix bugs

--- src/option.c
+++ src/option.c
@@ -1,5 +1,30 @@
 #include "../icl/proto.h"
 
+char	*sort_index(char *buf)
+{
+	char **arr = str_to_tab(buf, "\n:");
+	char **tab = NULL;
+	int	x = 0;
+	char tmp = '0';
+	char *str = NULL;
+
+	for (int i = 0; arr[i] != NULL; i++) {
+		if (i % 2 == 0) {
+			tab = str_to_tab(arr[i], ".");
+			tmp = x + '0';
+			tab[1] = ft_strcpy(&arr[i][2], &tmp);
+			arr[i] = NULL;
+			arr[i] = malloc(sizeof(char) * 1000);
+			ft_strcat(arr[i], tab[0]);
+			ft_strcat(arr[i], ".");
+			ft_strcat(arr[i], tab[1]);
+			x++;
+		}			
+	}
+	str = ft_strdup(tab_to_str(arr));
+	return (str);
+}
+
 void    begin_opt(char *file, char *id)
 {
 		char    **arr = NULL;
@@ -70,7 +95,7 @@ void    add_opt(char *file, char *task)
 		ft_strcat(new, task);
 		ft_strcat(new, "\n");
 		fd = open("./res/task.txt", O_WRONLY);
-		write(fd, new, ft_strlen(new));
+		write(fd, sort_index(new), ft_strlen(new) + 1);
 		free(new); 
 }
 
@@ -97,9 +122,8 @@ void	del_opt(char *file, char *id)
 		new = ft_strncpy(new, file, index + 2);
 		if (ft_strlen(&file[x]) > 3)
 			ft_strcat(new, &file[x]);
-		//printf("%s", new);
 		fd = open("./res/task.txt", O_WRONLY | O_TRUNC);
-		write(fd, new, ft_strlen(new));
+		write(fd, sort_index(new), ft_strlen(new) + 2);
 		free(new);
 }
 
